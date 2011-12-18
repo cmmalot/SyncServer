4dfa2d8cb25839d0f4c88150d5089baafb7e2d3b
Jan Magnusson <janne.magnusson@gmail.com>
2011-12-18 23:21:16 +0100

    Whitespace clean up

--- SyncServerTests.py
+++ SyncServerTests.py
@@ -11,19 +11,19 @@ import unittest
 import xmlrpclib
 
 class SyncServerTests(unittest.TestCase):
-	branch = "branch"
-	target = "target"
-	tag = "1.0.A.0.1"
-	
-	def setUp(self):
-		self.server = xmlrpclib.Server('http://localhost:8080')
+    branch = "branch"
+    target = "target"
+    tag = "1.0.A.0.1"
+    
+    def setUp(self):
+        self.server = xmlrpclib.Server('http://localhost:8080')
 
-	def test_GetApprovedManifest(self):
-		(success, txt) = self.server.GetApprovedManifest(self.branch)
-		self.assertTrue(success)
-		(success, txt) = self.server.GetApprovedManifest(self.branch, self.target)
-		self.assertTrue(success)
-		
+    def test_GetApprovedManifest(self):
+        (success, txt) = self.server.GetApprovedManifest(self.branch)
+        self.assertTrue(success)
+        (success, txt) = self.server.GetApprovedManifest(self.branch, self.target)
+        self.assertTrue(success)
+        
     
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
