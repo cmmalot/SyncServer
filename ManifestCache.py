04067483366d119af0cb99c20ab9f19a50cbdfd6
Jan Magnusson <janne.magnusson@gmail.com>
2012-01-05 16:02:05 +0100

    Added MemCache class to ManifestCache.py

--- ManifestCache.py
+++ ManifestCache.py
@@ -42,3 +42,38 @@ class Manifest:
 
     def __repr__(self):
         return "Manifest(name=%r, data=%s)" % (self.name, self.data)
+
+class MemCache:
+    def __init__(self, size=1):
+        self.size = size
+        self.items = []
+        self.item_data = {}
+
+    def store(self, key=None, value=None):
+        if key in self.items:
+            return
+        if len(self.items) <= self.size:
+            self.items.append(key)
+            self.item_data[key] = value
+        else:
+            self.remove(self.items[0])
+            self.items.append(key)
+            self.item_data[key] = value
+
+    def get(self, key=None):
+        if key in self.items:
+            self.refresh(key)
+            return self.item_data[key]
+
+    def remove(self, key=None):
+        if key in self.items:
+            self.items.remove(key)
+            del self.item_data[key]
+
+    def refresh(self, key=None):
+        if key in self.items:
+            if self.items.index(key) == (self.size -1):
+                self.items.remove(key)
+                self.items.append(key)
+
+    
